b91e4daa67eec170380bcb7798cf90fa5aee932a
Jeroen Degloire <jeroen.degloire@hotmail.be>
2020-06-12 19:38:30 +0200

    Update sanity project id.

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || "z3w69i7w",
-    dataset: process.env.GATSBY_SANITY_DATASET || "production"
-  }
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || "l2xxtj60",
+    dataset: process.env.GATSBY_SANITY_DATASET || "production",
+  },
 };
