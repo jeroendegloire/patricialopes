630350f61c9278c746157f03fd1b5630a928ad39
Jeroen Degloire <jeroen.degloire@hotmail.be>
2020-06-22 00:48:32 +0200

    seo

--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -1,72 +1,69 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import React from "react";
+import { Helmet } from "react-helmet";
+import { StaticQuery, graphql } from "gatsby";
 
 const SEO = ({ title, description, keywords, image, synonyms }) => (
-    <StaticQuery
-        query={query}
-        render={({
-            site: {
-                siteMetadata: {
-                    defaultDescription,
-                    defaultImage,
-                    url,
-                    defaultKeywords,
-                    defaultSynonyms,
-                    defaultTitle,
-                },
-            },
-        }) => {
-            //synonyms
-            const syno = synonyms || defaultSynonyms
-            const synkey = syno.map((syn) => syn)
+  <StaticQuery
+    query={query}
+    render={({
+      site: {
+        siteMetadata: {
+          defaultDescription,
+          defaultImage,
+          url,
+          defaultKeywords,
+          defaultSynonyms,
+          defaultTitle,
+        },
+      },
+    }) => {
+      //synonyms
+      const syno = synonyms || defaultSynonyms;
+      const synkey = syno.map((syn) => syn);
 
-            const seo = {
-                defaultTitle: defaultTitle,
-                description: description || defaultDescription,
-                image: `${image ? image : image || url + defaultImage}`,
-                keywords: `${
-                    keywords
-                        ? keywords + ',' + synkey
-                        : keywords + ',' + synkey ||
-                          defaultKeywords + defaultSynonyms
-                }`,
-            }
+      const seo = {
+        defaultTitle: defaultTitle,
+        description: description || defaultDescription,
+        image: `${image ? image : image || url + defaultImage}`,
+        keywords: `${
+          keywords
+            ? keywords + "," + synkey
+            : keywords + "," + synkey || defaultKeywords + defaultSynonyms
+        }`,
+      };
 
-            return (
-                <Helmet title={title} titleTemplate={`%s | ${defaultTitle}`}>
-                    <html lang='en' />
-                    <meta name='image' content={seo.image} />
-                    <meta name='description' content={seo.description} />
-                    <meta name='keywords' content={seo.keywords} />
-                    <meta property='og:title' content={title} />
-                    <meta property='og:description' content={seo.description} />
-                    <meta property='og:image' content={seo.image} />
-                    <meta name='twitter:title' content={title} />
-                    <meta
-                        name='twitter:description'
-                        content={seo.description}
-                    />
-                    <meta name='twitter:image' content={seo.image} />
-                </Helmet>
-            )
-        }}
-    />
-)
+      return (
+        <Helmet title={title} titleTemplate={`%s | ${defaultTitle}`}>
+          <html lang="en" />
+          <meta name="image" content={seo.image} />
+          <meta name="description" content={seo.description} />
+          <meta name="keywords" content={seo.keywords} />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={seo.description} />
+          <meta property="og:image" content={seo.image} />
+          <meta name="twitter:title" content={title} />
+          <meta name="twitter:description" content={seo.description} />
+          <meta name="twitter:image" content={seo.image} />
+          <link rel="preconnect" href="https://vimeo.com/161841346"></link>
+        </Helmet>
+      );
+    }}
+  />
+);
 
-export default SEO
+export default SEO;
 
 const query = graphql`
-    {
-        site {
-            siteMetadata {
-                defaultTitle: title
-                defaultDescription: description
-                defaultImage: image
-                url
-                defaultKeywords: keywords
-                defaultSynonyms: synonyms
-            }
-        }
+  {
+    site {
+      siteMetadata {
+        defaultTitle: title
+        defaultDescription: description
+        defaultImage: image
+        url
+        defaultKeywords: keywords
+        defaultSynonyms: synonyms
+      }
     }
-`
+  }
+`;
