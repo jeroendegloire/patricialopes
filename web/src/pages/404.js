bc9420e7cf00765c91d26d28d01b3f84f66160b9
Jeroen Degloire <jeroen.degloire@hotmail.be>
2020-07-01 06:37:45 +0200

    404

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,25 +1,20 @@
-import React from 'react'
+import React from "react";
 
-import Layout from '../components/layout/layout'
-import SEO from '../components/seo'
-import abductionIllustration from '../images/abduction-illustration.svg'
+import Layout from "../components/layout/layout";
+import SEO from "../components/seo";
+import abductionIllustration from "../images/abduction-illustration.svg";
 
 function NotFoundPage() {
-    return (
-        <Layout>
-            <SEO title='404: Not found' />
-            <div>
-                <img
-                    alt='Ghost getting abducted by aliens'
-                    className='block mx-auto w-1/2'
-                    src={abductionIllustration}
-                />
-                <h2 className='bg-yellow-400 text-2xl font-bold inline-block my-8 p-3'>
-                    404 text
-                </h2>
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <div className="flex-1 items-center flex">
+        <h2 className="text-4xl font-bold inline-block my-8 p-3 mx-auto">
+          404 Page not found
+        </h2>
+      </div>
+    </Layout>
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;
