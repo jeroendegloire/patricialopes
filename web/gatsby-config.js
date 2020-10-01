7eca6fe7683fdd94494449e64b888001cd58ab8a
Jeroen Degloire <jeroen.degloire@hotmail.be>
2020-10-01 12:34:32 +0200

    env variables

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -18,10 +18,18 @@ module.exports = {
       resolve: "gatsby-source-sanity",
       options: {
         projectId: process.env.SANITY_PROJECT_ID,
-        dataset: process.env.SANITY_PROJECT_DATASET,
-        overlayDrafts: !isProd,
-        watchMode: !isProd,
-        token: process.env.SANITY_TOKEN,
+        dataset: process.env.SANITY_DATASET,
+        ...(process.env.SANITY_READ_TOKEN && {
+          token: process.env.SANITY_READ_TOKEN,
+        }),
+        ...(process.env.SANITY_WATCH_MODE &&
+          process.env.SANITY_READ_TOKEN && {
+            watchMode: process.env.SANITY_WATCH_MODE,
+          }),
+        ...(process.env.SANITY_OVERLAY_DRAFTS &&
+          process.env.SANITY_READ_TOKEN && {
+            overlayDrafts: process.env.SANITY_OVERLAY_DRAFTS,
+          }),
       },
     },
     {
