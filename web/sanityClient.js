f5abb1aa6fee2fefb0e8cbe4cba7b7ab1727fc75
Jeroen Degloire <jeroen.degloire@hotmail.be>
2020-10-01 16:52:47 +0200

    Preview

--- web/sanityClient.js
+++ web/sanityClient.js
@@ -15,6 +15,9 @@ export const productionClient = sanityClient({
 });
 
 console.log(process.env.NODE_ENV);
+console.log(process.env.ENABLE_GATSBY_REFRESH_ENDPOINT);
+console.log(process.env.SANITY_OVERLAY_DRAFTS);
+console.log(process.env.SANITY_WATCH_MODE);
 
 const client =
   process.env.NODE_ENV == "development" ? previewClient : productionClient;
