001ffc8ae7dab9eb5d62b73290643deb0251fc63
Jeroen Degloire <jeroen.degloire@hotmail.be>
2020-10-01 14:26:37 +0200

    preview env

--- web/sanityClient.js
+++ web/sanityClient.js
@@ -3,14 +3,14 @@ import sanityClient from "@sanity/client";
 
 export const previewClient = sanityClient({
   projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_PROJECT_DATASET,
+  dataset: process.env.SANITY_DATASET,
   useCdn: false,
   withCredentials: true,
 });
 
 export const productionClient = sanityClient({
   projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_PROJECT_DATASET,
+  dataset: process.env.SANITY_DATASET,
   useCdn: true,
 });
 
